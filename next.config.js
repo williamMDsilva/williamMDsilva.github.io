4833365a5e7d5b4bfdc6c21a61dc66bd19dd46d0
William Moreira da Silva <william.moreirasilva@hotmail.com>
2020-10-12 00:56:54 -0300

    Deploy gh-pages

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     }
   },
   //assetPrefix: '',
-  assetPrefix: !debug ? '/' : '',
+  assetPrefix: !debug ? '/williammdsilva.github.io/' : '',
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
